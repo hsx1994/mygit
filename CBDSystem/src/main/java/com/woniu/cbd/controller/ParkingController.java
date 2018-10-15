7c11e8b5a3ea144a2a6e93fed5ab31970f735f45
胡盛熙 <940695965@qq.com>
2018-10-15 14:19:20 +0800

    添加了Parking接口，实现和controller中的方法

--- CBDSystem/src/main/java/com/woniu/cbd/controller/ParkingController.java
+++ CBDSystem/src/main/java/com/woniu/cbd/controller/ParkingController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.woniu.cbd.bean.ParkingBean;
 import com.woniu.cbd.service.IParkingService;
@@ -49,5 +51,48 @@ public class ParkingController {
 		return null;
 
 	}
+	
 
+	@RequestMapping("/15")
+	public @ResponseBody String parkingDelete(Integer id){
+		String result = "删除失败";
+		boolean re = park.parkingDelete(id);
+		if(re){
+			result = "删除成功";
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/16")
+	public ModelAndView parkingSelect(){
+		ModelAndView mav = new ModelAndView();
+		List<ParkingBean> list = park.parkingSelect();
+		mav.addObject("allParking",list);
+		mav.setViewName("");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/17")
+	public @ResponseBody String passApply(Integer id){
+		String result = "通过失败";
+		boolean re = park.passApply(id);
+		if(re){
+			result = "通过成功";
+		}
+		
+		return result;
+	}
+
+	@RequestMapping("/18")
+	public @ResponseBody String passApplyFail(Integer id){
+		String result = "失败";
+		boolean re = park.passApplyFail(id);
+		if(re){
+			result = "成功";
+		}
+		
+		return result;
+	}
 }
--- CBDSystem/src/main/java/com/woniu/cbd/service/impl/ParkingServiceimpl.java
+++ CBDSystem/src/main/java/com/woniu/cbd/service/impl/ParkingServiceimpl.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.woniu.cbd.bean.ComplainBean;
-import com.woniu.cbd.bean.OrderBean;
 import com.woniu.cbd.bean.ParkingBean;
 import com.woniu.cbd.dao.IParkingDao;
 import com.woniu.cbd.service.IParkingService;
@@ -68,5 +66,38 @@ public class ParkingServiceimpl implements IParkingService {
 		// TODO Auto-generated method stub
 		List<ParkingBean> bean=dao.SelectParkByNum(num);
 		return bean;
+	}
+	
+	@Override
+	public boolean parkingDelete(int id) {
+		int row = dao.parkingDelete(id);
+		if(row > 0){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<ParkingBean> parkingSelect() {
+		List<ParkingBean> list = dao.parkingSelect();
+		return list;
+	}
+
+	@Override
+	public boolean passApply(int id) {
+		int row = dao.passApply(id);
+		if(row > 0){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean passApplyFail(int id) {
+		int row = dao.passApplyFail(id);
+		if(row > 0){
+			return true;
+		}
+		return false;
 	}
 }
