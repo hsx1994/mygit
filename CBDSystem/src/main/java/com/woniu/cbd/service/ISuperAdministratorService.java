df9c94960badd92fccb4ffff2e14ccecb9d70c8a
TaoLiu <1039303893@qq.com>
2018-10-21 10:06:41 +0800

    提交

---
+++ CBDSystem/src/main/java/com/woniu/cbd/service/ISuperAdministratorService.java
@@ -0,0 +1,12 @@
+package com.woniu.cbd.service;
+
+public interface ISuperAdministratorService {
+
+	/**
+	 * 根据登录表查超级管理员id
+	 * @param lid
+	 * @return
+	 */
+	public int findIdByLid(int lid);
+
+}
